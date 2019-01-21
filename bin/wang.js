1b4e92740fa719df1613d469f6e68e419ff0647b
wangServer <345619747@qq.com>
2019-01-22 00:29:55 +0800

    dev

--- bin/wang.js
+++ bin/wang.js
@@ -1,17 +1,22 @@
 #! /usr/bin/env node
 
 const program = require('commander');
-const chalk = require('chalk');
 const app = require('../lib/server');
 const copy = require('../lib/copy');
-
 const fs = require('fs');
 const path = require('path');
 const os = require('os');
-
 const currentPath = process.cwd();
+const {
+  console_normal,
+  console_warn,
+  console_ok
+} = require('../lib/common/tools')
+const package = require('../package.json')
+const version = package.version
+
 // 版本命令
-program.version('0.0.11');
+program.version(version);
 
 program
   .command('help')
@@ -26,37 +31,35 @@ program
   .action(function () {
     // 获取用户端口
     let filepath = path.resolve(process.cwd(), 'config.json');
-    let port = 3000;
-    fs.access(filepath, fs.constants.F_OK, (err) => {
-      if (err) {
-        // 文件不存在
-        console.log(chalk.yellow(`不存在config.json文件，使用默认设置`))
-        console.log(chalk.yellow(`关闭服务器，使用 "wang init" 可以生成默认配置文件 `))
-      } else {
-        // 文件存在
-        let CONFIG = JSON.parse(fs.readFileSync(path.resolve(process.cwd(), 'config.json'))) || {};
-        port = CONFIG.port || 3000;
-        console.log(chalk.bgGreen(`存在config.json文件，可以在文件中设置端口或者虚拟api接口`))
-      }
-      let server = app.listen(port)
-      console.log(chalk.green(`静态服务器启动`))
-      console.log(chalk.green(`路径：${process.cwd()}`))
-      console.log(chalk.green(`端口：${port}`))
-      let ipinfo = getipv4();
-      if (ipinfo['本地连接']) {
-        console.log(chalk.green(`本地IP：${ipinfo['本地连接'][1].address}`))
-      }else if (ipinfo['以太网']) {
-        console.log(chalk.green(`本地IP：${getipv4()['以太网'][1].address}`))
-      }
-      console.log(chalk.green(`日志： `))
-    })
+    let defaultPort = 3000;
+   
+    if (fs.existsSync(filepath)) {
+      let CONFIG = JSON.parse(fs.readFileSync(path.resolve(process.cwd(), 'config.json'))) || {};
+      defaultPort = CONFIG.port || defaultPort;
+      console_ok(`存在config.json文件，可以在文件中设置端口或者虚拟api接口`)
+    } else {
+      console_warn(`不存在config.json文件，使用默认设置`)
+      console_warn(`关闭服务器，使用 "wang init" 可以生成默认配置文件 `)
+    }
+
+    let server = app.listen(defaultPort)
+    console_normal(`静态服务器启动`)
+    console_normal(`路径：${process.cwd()}`)
+    console_normal(`端口：${defaultPort}`)
+    let ipinfo = getipv4();
+    if (ipinfo['本地连接']) {
+      console_normal(`本地IP：${ipinfo['本地连接'][1].address}`)
+    } else if (ipinfo['以太网']) {
+      console_normal(`本地IP：${getipv4()['以太网'][1].address}`)
+    }
+    console_normal(`日志： `)
   })
 
 program
   .command('init')
   .description('生成config.json文件模板')
   .action(function () {
-    copy(path.resolve(__dirname,'../lib/config.json'),currentPath);
+    copy(path.resolve(__dirname, '../lib/config.json'), currentPath);
   })
 
 program
